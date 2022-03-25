4c87d9332af229b2736d41110897c8d9a18df072
Sarkan Mohammady <sarkan.mohammady@hotmail.com>
2022-03-25 18:00:10 +0430

    router starter implemented

--- app/starter.go
+++ app/starter.go
@@ -1,11 +1,16 @@
 package app
 
 import (
+	"context"
+	"log"
+	"time"
+
 	"github.com/9sarkan/golang-base/config"
+	"github.com/9sarkan/golang-base/pkg/middlewares"
+	"github.com/9sarkan/golang-base/pkg/utils"
 )
 
-type App struct {
-}
+type App struct{}
 
 // StartApplication function will start application and configure all dependencies
 func (app App) StartApplication(dbg bool, cfgPath string) error {
@@ -13,10 +18,24 @@ func (app App) StartApplication(dbg bool, cfgPath string) error {
 	if err := config.Map.LoadFromFile(cfgPath); err != nil {
 		return err
 	}
+	log.Default().Println("config loaded.")
+	if err := app.StartServers(); err != nil {
+		return err
+	}
 	return nil
 }
 
 // StartDatabases configure databases
 func (app App) StartDatabases(cnf config.Database) error {
+	return nil
+}
+
+func (app App) StartServers() error {
+	// start grpc server
+	rc := RouterCenter{}
+	if err := rc.InitialGrpcServer(context.Background(), config.Map.Service.GRPCServer, middlewares.GRPCAuth, utils.PanicRecover); err != nil {
+		return err
+	}
+	time.Sleep(time.Hour)
 	return nil
 }
