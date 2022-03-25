125cfcf8302412ae36c4bbbb352604514f64a96a
Sarkan Mohammady <sarkan.mohammady@hotmail.com>
2022-03-25 17:57:44 +0430

    oauth package added

--- go.mod
+++ go.mod
@@ -3,8 +3,13 @@ module github.com/9sarkan/golang-base
 go 1.18
 
 require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/google/uuid v1.1.2
+	github.com/grpc-ecosystem/go-grpc-middleware v1.3.0
+	github.com/grpc-ecosystem/go-grpc-prometheus v1.2.0
 	github.com/spf13/cobra v1.4.0
 	github.com/spf13/viper v1.10.1
+	google.golang.org/grpc v1.43.0
 	google.golang.org/protobuf v1.27.1
 )
 
@@ -18,13 +23,23 @@ require (
 	github.com/spf13/cast v1.4.1 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/subosito/gotenv v1.2.0 // indirect
-	golang.org/x/sys v0.0.0-20211210111614-af8b64212486 // indirect
+	golang.org/x/sys v0.0.0-20220114195835-da31bd327af9 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	gopkg.in/ini.v1 v1.66.2 // indirect
 )
 
 require (
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
+	github.com/prometheus/client_golang v1.12.1 // indirect
+	github.com/prometheus/client_model v0.2.0 // indirect
+	github.com/prometheus/common v0.32.1 // indirect
+	github.com/prometheus/procfs v0.7.3 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/net v0.0.0-20210813160813-60bc85c4be6d // indirect
+	google.golang.org/genproto v0.0.0-20211208223120-3a66f561d7aa // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
---
+++ pkg/oauth/jwt.go
@@ -0,0 +1,75 @@
+package oauth
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log"
+
+	"github.com/9sarkan/golang-base/config"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/google/uuid"
+)
+
+var (
+	JWT oautInterface = &AccessDetails{}
+)
+
+type oautInterface interface {
+	VerifyToken(ctx context.Context, request string) (*AccessDetails, error)
+}
+
+// AccessDetails struct
+type AccessDetails struct {
+	UserID string
+	Dialer string
+}
+
+func (a *AccessDetails) VerifyToken(ctx context.Context, request string) (*AccessDetails, error) {
+	token, err := jwt.Parse(request, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); ok {
+			return []byte(config.Map.AuthCreditional.HMACSecret), nil
+		}
+		if _, ok := token.Method.(*jwt.SigningMethodRSA); ok {
+			return jwt.ParseRSAPublicKeyFromPEM([]byte(config.Map.AuthCreditional.RSASecret))
+		}
+		if _, ok := token.Method.(*jwt.SigningMethodECDSA); ok {
+			return jwt.ParseECPublicKeyFromPEM([]byte(config.Map.AuthCreditional.ECDSASecret))
+		}
+
+		log.Default().Println(fmt.Sprintf("unexpected signing method: %v", token.Header["alg"]))
+		return nil, errors.New("401 unauthorized")
+	})
+	if err != nil {
+		return nil, errors.New("401 unauthorized")
+	}
+	if token.Claims == nil || !token.Valid {
+		return nil, errors.New("401 unauthorized")
+	}
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if ok && token.Valid {
+		userID, ok := claims["sub"].(string)
+		if !ok {
+			log.Default().Println(fmt.Sprintf("error in get user sub from token: %v", claims))
+			return nil, errors.New("401 unauthorized")
+		}
+
+		if _, err := uuid.Parse(userID); err != nil {
+			log.Default().Println(fmt.Sprintf("error in sub typo: %v", err))
+			return nil, errors.New("401 unauthorized")
+		}
+
+		dialer, ok := claims["mobile"].(string)
+		if !ok {
+			log.Default().Println(fmt.Sprintf("error in get user mobile from token: %v", claims))
+			return nil, errors.New("401 unauthorized")
+		}
+
+		return &AccessDetails{
+			UserID: userID,
+			Dialer: dialer,
+		}, nil
+	}
+	return nil, err
+}
