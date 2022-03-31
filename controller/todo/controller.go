67d52f4cd1ee8f126a9853385db45f0bb11327c7
Sarkan Mohammady <sarkan.mohammady@hotmail.com>
2022-03-31 04:57:08 +0430

    mock data added

--- controller/todo/controller.go
+++ controller/todo/controller.go
@@ -2,8 +2,10 @@ package todo
 
 import (
 	"context"
+	"time"
 
 	justgrpcservice "github.com/9sarkan/golang-base/api/pb/github.com/9sarkan/golang-base/pkg/pb/justGRPCService.proto"
+	"github.com/google/uuid"
 	"google.golang.org/protobuf/types/known/emptypb"
 )
 
@@ -16,5 +18,16 @@ func (Controller) Create(ctx context.Context, request *justgrpcservice.CreateToD
 }
 
 func (Controller) Get(ctx context.Context, _ *emptypb.Empty) (*justgrpcservice.ToDoListResponse, error) {
-	return nil, nil
+	// mock service for test
+	return &justgrpcservice.ToDoListResponse{
+		Count: 1,
+		Data: []*justgrpcservice.ToDoSignleResponse{
+			{
+				Id:        uuid.New().String(),
+				Title:     "test",
+				CreatedAt: time.Now().String(),
+				Body:      "test body",
+			},
+		},
+	}, nil
 }
